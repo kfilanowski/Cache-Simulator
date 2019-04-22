1be108acc4195b0ffb92b6a54a431c54f6bef6db
Kevin <kevinfilanowski@gmail.com>
2019-04-22 17:30:25 -0400

    skeleton created

---
+++ Driver.java
@@ -0,0 +1,19 @@
+/**
+ * // TODO
+ * 
+ * @author Kevin Filanowski
+ * @author Caleb Tupone
+ * @version April 22, 2019
+ */
+public class Driver {
+
+    /**
+     * // TODO
+     * 
+     * @param args - 
+     */
+    public static void main(String[] args) {
+        Simulator sim = new Simulator();
+        sim.go();
+    }
+}
---
+++ Simulator.java
@@ -0,0 +1,14 @@
+/**
+ * // TODO
+ * 
+ * @author Kevin Filanowski
+ * @author Caleb Tupone
+ * @version April 22, 2019
+ */
+public class Simulator {
+    
+    public void go() {
+
+    }
+
+}
